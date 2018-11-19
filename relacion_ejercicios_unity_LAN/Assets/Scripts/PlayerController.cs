afaf2fabb652ddb4f0649dd904096288dc77197c
Lucía Arco Núñez <diseno@iMac-de-Diseno-230.local>
2018-11-19 13:16:59 +0100

    WASD movimiento
    1.a

---
+++ relacion_ejercicios_unity_LAN/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public int fuerza = 10;
+    int fuerzaSalto = 30;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * fuerza);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector2.right * fuerza);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * fuerzaSalto);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector2.down * fuerza);
+        }
+    }
+}
