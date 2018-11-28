8846b69c56b608989af5de75a57df86e8cf153be
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2018-11-28 19:41:22 +0100

    Pausa /Start + arreglo contador

---
+++ relacion_ejercicios_unity_LAN/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+    public Animator animInterface;
+    public bool pausa = false;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausa = !pausa; //cuando pulsas esc pausa se vuelve true, o viceversa
+            animInterface.SetBool("activar", pausa);
+        }
+
+        if (pausa)
+        {
+            Time.timeScale = 0f; //o a 0.5f para que vaya muy lento
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+
+    }
+}
